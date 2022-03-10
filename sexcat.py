8a5bda59cfb0ad77e0b5a2488ceaf57616e353b8
kunal pai <62979320+kunpai@users.noreply.github.com>
2022-03-10 00:59:34 -0800

    Update sexcat.py
    Added support for two slash commands

--- sexcat.py
+++ sexcat.py
@@ -1,15 +1,26 @@
 import discord
 import os
 import random
+import interactions
+from discord_slash import SlashCommand, SlashContext
 from dotenv import load_dotenv
 load_dotenv()
 
 client = discord.Client()
+slash = SlashCommand(client, sync_commands=True)
 
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
     os.chdir("./sexcat")
+    
+@slash.slash(name ="meow", description="Sends sexcat photo")
+async def _meow (ctx = SlashContext):
+    await ctx.send(file=discord.File(random.choice(os.listdir(os.getcwd()))))
+
+@slash.slash(name ="answer", description="Solves your life for you")
+async def _answer (ctx = SlashContext):
+    await ctx.send('Can')
 
 @client.event
 async def on_message(message):
