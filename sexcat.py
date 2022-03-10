44a8f2005fe6a3ad39ae3f04466549beed483241
kunal pai <62979320+kunpai@users.noreply.github.com>
2022-03-09 16:40:28 -0800

    Add files via upload

---
+++ sexcat.py
@@ -0,0 +1,24 @@
+import discord
+import os
+import random
+from dotenv import load_dotenv
+load_dotenv()
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+    os.chdir("./sexcat")
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('!'):
+        await message.channel.send('meow meow I just want can')
+        await message.channel.send(file=discord.File(random.choice(os.listdir(os.getcwd()))))
+
+client.run(os.getenv('DISCORD_TOKEN'))
+
